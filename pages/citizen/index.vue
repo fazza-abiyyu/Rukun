ee9fd36f0df34e41363896444cfb67ae8adab484
unknown <ellnaufall@gmail.com>
2025-02-11 15:12:18 +0700

    update sidebar,logo,header

--- pages/citizen/index.vue
+++ pages/citizen/index.vue
@@ -29,7 +29,7 @@
           </svg>
         </li>
         <li class="text-sm font-semibold text-gray-800 truncate" aria-current="page">
-          Puskesmas
+          Data Warga
         </li>
       </ol>
       <!-- End Breadcrumb -->
